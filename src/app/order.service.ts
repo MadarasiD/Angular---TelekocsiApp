fa794c3cd72baa313b06a7f541ba85ef59de1733
MadarasiD <madarka0913@gmail.com>
2022-07-22 23:02:21 +0200

    update

--- src/app/order.service.ts
+++ src/app/order.service.ts
@@ -1,38 +1,42 @@
 import { Injectable } from '@angular/core';
 import { LoginModel } from './login-model';
-import { HttpClient }  from '@angular/common/http';
-import { TokenModel } from './token-model'
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { TokenModel } from './token-model';
 import { Router } from '@angular/router';
-import { Order } from './order';
+import {  Order } from './order';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
   public loginmodel: LoginModel = new LoginModel();
+  public currentOrder: Order = new Order();
   http: HttpClient;
   router: Router;
-  public orders:Array<Order> = new Array<Order>();
-  
-  constructor(http: HttpClient, router: Router) { 
+  public orders: Array<Order> = new Array<Order>();
+
+  constructor(http: HttpClient, router: Router) {
     this.http = http;
     this.router = router;
   }
 
   public login(){
     this.http.post<TokenModel>('https://weblerapi.kovos.net/auth', this.loginmodel)
-    .subscribe(t => { localStorage.setItem('ordertoken', t.token);
-    this.router.navigate(['/browse']);
-  });
+    .subscribe(t => {
+      localStorage.setItem('ordertoken', t.token);
+      localStorage.setItem('username', this.loginmodel.username);
+      this.router.navigate(['/browse']);
+    });
   }
 
   public isLoggedIn(): boolean {
     return localStorage.getItem('ordertoken') != null;
   }
 
-  public logout() {
+  public logout(){
     localStorage.removeItem('ordertoken');
-    this.router.navigate(['/login']);
+    localStorage.removeItem('username');
+    this.router.navigate(['/browse']);
   }
 
   public disableGuest(){
@@ -54,5 +58,28 @@ export class OrderService {
       console.log(this.orders);
     });
   }
+
+  public removeORder(id: string){
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${localStorage.getItem('ordertoken')}`
+    });
+
+    this.http.delete('https://weblerapi.kovos.net/car/' + id,
+     {headers: headers})
+    .subscribe(t => this.getOrders());
+  }
+
+  public createOrder(){
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${localStorage.getItem('ordertoken')}`
+    });
+    this.http.post('https://weblerapi.kovos.net/car', this.currentOrder, 
+    {headers: headers})
+    .subscribe(t => this.router.navigate(['/browse']));
+  }
+
+  
 
 }
